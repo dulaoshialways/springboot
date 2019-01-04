5ec7a624e7ef5616dc380fa1342463065eed4e25
hemisi <hemisi@swiftdata.cn>
2019-01-04 17:53:03 +0800

    修改打包配置

--- springboot-all-consumer/src/main/java/com/du/springboot/controller/IndexController.java
+++ springboot-all-consumer/src/main/java/com/du/springboot/controller/IndexController.java
@@ -1,12 +1,18 @@
 package com.du.springboot.controller;
 
+import com.alibaba.dubbo.config.annotation.Reference;
 import com.du.springboot.entity.User;
+import com.du.springboot.service.UserService;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 
 /**
@@ -16,6 +22,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class IndexController {
+
+    @Reference
+    private UserService userService;
 
     @RequestMapping("/login.html")
     public String toLogin(){
@@ -30,6 +39,11 @@ public class IndexController {
             UsernamePasswordToken token = new UsernamePasswordToken(username,passWord);
             subject.login(token);
             //跳转到登录成功页面
+            Map<String,Object> map = new ConcurrentHashMap<>();
+            map.put("start",0);
+            map.put("pageSize",10);
+            List<User> userList = userService.getUserByPage(map);
+            model.addAttribute("userList",userList);
             return "user";
         } catch (UnknownAccountException e){
             e.printStackTrace();
